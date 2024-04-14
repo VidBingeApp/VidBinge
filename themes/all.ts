8acf06202e1142f5df5c939995cb21e99106fd91
Josh Holly <joshholly@gmail.com>
2024-04-14 05:47:38 -0500

    Deleted Blue theme since now default

--- themes/all.ts
+++ themes/all.ts
@@ -1,11 +1,9 @@
 import teal from "./list/teal";
-import blue from "./list/blue";
 import red from "./list/red";
 import gray from "./list/gray";
 
 export const allThemes = [
   teal,
-  blue,
   gray,
   red
 ]
