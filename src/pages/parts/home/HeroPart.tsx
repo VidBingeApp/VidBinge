bd5cfa350d288896f0db950f055d157ffbe7b1eb
Josh Holly <joshholly@gmail.com>
2024-04-30 01:52:39 -0500

    Added text-wrap: pretty; to Hero section

--- src/pages/parts/home/HeroPart.tsx
+++ src/pages/parts/home/HeroPart.tsx
@@ -67,7 +67,9 @@ export function HeroPart({ setIsSticky, searchParams }: HeroPartProps) {
     <ThinContainer>
       <div className="mt-44 space-y-16 text-center">
         <div className="relative z-10 mb-16">
-          <HeroTitle className="mx-auto max-w-md">{title}</HeroTitle>
+          <HeroTitle className="mx-auto text-pretty max-w-md">
+            {title}
+          </HeroTitle>
         </div>
         <div className="relative h-20 z-30">
           <Sticky
