8ba2e4d0cd7c4fe1f7754b06f08ed8f249479721
Josh Holly <joshholly@gmail.com>
2024-05-26 01:32:59 -0500

    Updated DISCORD_LINK and GITHUB_LINK in constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,6 +1,6 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
-export const DISCORD_LINK = "https://movie-web.github.io/links/discord";
-export const GITHUB_LINK = "https://github.com/movie-web/movie-web";
+export const DISCORD_LINK = "https://t.me/vidbinge";
+export const GITHUB_LINK = "https://github.com/joshholly/VidBinge";
 export const DONATION_LINK = "https://lnk.bio/wafflehacker";
 export const GA_ID = import.meta.env.VITE_GA_ID;
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
