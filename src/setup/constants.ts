421787771876d0f8477dfd17d39d761b774456f7
Josh Holly <joshholly@gmail.com>
2024-07-05 00:59:10 -0500

    Change donate link

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,7 +1,7 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
 export const DISCORD_LINK = "https://discord.gg/4jzBvV8E5r";
 export const GITHUB_LINK = "https://github.com/joshholly/VidBinge";
-export const DONATION_LINK = "https://lnk.bio/wafflehacker";
+export const DONATION_LINK = "https://ko-fi.com/vidbinge";
 export const FACEBOOK_LINK = "https://facebook.com/vidbinge";
 export const INSTAGRAM_LINK = "https://instagram.com/vidbinge";
 export const GA_ID = import.meta.env.VITE_GA_ID;
