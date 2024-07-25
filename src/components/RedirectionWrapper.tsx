523f5d6719e68b266c47f541c53a55135dacf996
Josh Holly <joshholly@gmail.com>
2024-07-25 00:29:10 -0500

    Made SF Redirection Wrapper mobile responsive.

--- src/components/RedirectionWrapper.tsx
+++ src/components/RedirectionWrapper.tsx
@@ -18,11 +18,11 @@ export function RedirectionWrapper({ children }: RedirectionWrapperProps) {
     <>
       {showPopup && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50">
-          <div className="bg-purple-950 p-8 rounded-xl shadow-2xl text-center text-white max-w-2xl w-full max-h-[90vh] overflow-y-auto">
-            <h2 className="text-3xl font-bold mb-6 text-purple-300">
+          <div className="bg-purple-950 p-4 md:p-8 rounded-xl shadow-2xl text-center text-white w-11/12 md:w-3/4 lg:w-1/2 xl:w-2/5 2xl:max-w-2xl max-h-[90vh] overflow-y-auto">
+            <h2 className="text-xl md:text-3xl font-bold mb-4 md:mb-6 text-purple-300">
               Welcome to VidBinge.com!
             </h2>
-            <div className="space-y-4 text-gray-300 text-sm leading-relaxed">
+            <div className="space-y-3 md:space-y-4 text-sm md:text-base leading-relaxed text-gray-300">
               <p>
                 Hello there, On July 25th 2024 Sudo-Flix.lol announced its
                 closure due to a Cease and Desist Letter. You can discover more
@@ -62,7 +62,7 @@ export function RedirectionWrapper({ children }: RedirectionWrapperProps) {
             </div>
             <button
               type="button"
-              className="mt-6 px-6 py-2 bg-purple-700 text-white rounded-full hover:bg-purple-600 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50"
+              className="mt-4 md:mt-6 px-5 md:px-6 py-2 text-sm md:text-base bg-purple-700 text-white rounded-full hover:bg-purple-600 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50"
               onClick={() => setShowPopup(false)}
             >
               Close
