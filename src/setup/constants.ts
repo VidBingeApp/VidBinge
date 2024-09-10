4e03b43dcd1700100badd3357606be3347df8c52
Josh Holly <joshholly@gmail.com>
2024-09-10 02:03:41 -0500

    Changed Github link

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,6 +1,6 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
 export const DISCORD_LINK = "https://discord.gg/4jzBvV8E5r";
-export const GITHUB_LINK = "https://github.com/joshholly/VidBinge";
+export const GITHUB_LINK = "https://github.com/VidBingeApp/VidBinge";
 export const DONATION_LINK = "https://ko-fi.com/vidbinge";
 export const FACEBOOK_LINK = "https://facebook.com/vidbinge";
 export const INSTAGRAM_LINK = "https://instagram.com/vidbinge";
